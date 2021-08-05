1c03a0fb712f636b918add5ec88c8d812cd5f4d2
JasonZhang29 <59625134+JasonZhang29@users.noreply.github.com>
2021-08-04 22:48:56 -0400

    Add files via upload

---
+++ create.sql
@@ -0,0 +1,151 @@
+CREATE TABLE IF NOT EXISTS person(
+	p_id INT AUTO_INCREMENT NOT NULL,
+	first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    dob DATE,
+    phone CHAR(10),
+    address VARCHAR(100),
+    city VARCHAR(50),
+    province CHAR(2),
+    postal_code CHAR(6),
+    email VARCHAR(50),
+    PRIMARY KEY(p_id)
+);
+
+CREATE TABLE  IF NOT EXISTS non_citizen(
+	p_id INT NOT NULL,
+    passport VARCHAR(50) NOT NULL,
+    PRIMARY KEY(p_id),
+    FOREIGN KEY(p_id) REFERENCES person(p_id)
+);
+
+CREATE TABLE IF NOT EXISTS citizen(
+	p_id INT NOT NULL,
+    ssn CHAR(9) NOT NULL,
+    medicare CHAR(12) NOT NULL,
+    PRIMARY KEY(p_id),
+    FOREIGN KEY(p_id) REFERENCES person(p_id)
+);
+
+CREATE TABLE IF NOT EXISTS health_worker(
+	p_id INT NOT NULL,
+    emp_id INT NOT NULL UNIQUE,
+    PRIMARY KEY(p_id),
+    FOREIGN KEY(p_id) REFERENCES citizen(p_id)
+);
+
+CREATE TABLE IF NOT EXISTS age_group(
+	grp_id SMALLINT NOT NULL,
+    upper_limit SMALLINT,
+    lower_limit SMALLINT,
+    PRIMARY KEY(grp_id)
+);
+
+CREATE TABLE IF NOT EXISTS pv_age(
+	province CHAR(2) NOT NULL,
+    grp_id SMALLINT DEFAULT 0,
+    PRIMARY KEY(province),
+    FOREIGN KEY(grp_id) REFERENCES age_group(grp_id)
+);
+
+CREATE TABLE IF NOT EXISTS facility(
+	loc_id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(50),
+    address VARCHAR(50),
+    city VARCHAR(20),
+    province CHAR(2),
+    postal_code CHAR(6),
+    phone CHAR(10),
+    web VARCHAR(100),
+    type VARCHAR(20),
+    manager INT UNIQUE,
+    PRIMARY KEY(loc_id),
+    FOREIGN KEY(manager) REFERENCES citizen(p_id),
+    FOREIGN KEY(province) REFERENCES pv_age(province)
+);
+
+CREATE TABLE IF NOT EXISTS work_history(
+	emp_id INT NOT NULL,
+    loc_id INT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE,
+    PRIMARY KEY(emp_id, loc_id, start_date),
+    FOREIGN KEY(loc_id) REFERENCES facility(loc_id),
+    FOREIGN KEY(emp_id) REFERENCES health_worker(emp_id)
+);
+
+CREATE TABLE IF NOT EXISTS vaccine(
+	vac_id SMALLINT NOT NULL,
+    name VARCHAR(50),
+    vac_desc VARCHAR(100),
+    status ENUM ('safe','suspend'),
+    PRIMARY KEY(vac_id)
+);
+
+CREATE TABLE IF NOT EXISTS vaccine_history(
+	vac_id SMALLINT NOT NULL,
+    update_date DATE NOT NULL,
+    update_status ENUM ('safe','suspend') NOT NULL,
+    PRIMARY KEY(vac_id, update_date, update_status)
+);
+
+CREATE TABLE IF NOT EXISTS vaccination(
+	p_id INT NOT NULL,
+    dose_num SMALLINT NOT NULL,
+    emp_id INT NOT NULL,
+    vac_id SMALLINT NOT NULL,
+    loc_id INT NOT NULL,
+    vdate DATE NOT NULL,
+    PRIMARY KEY(p_id, dose_num),
+    FOREIGN KEY(emp_id) REFERENCES health_worker(emp_id),
+    FOREIGN KEY(vac_id) REFERENCES vaccine(vac_id),
+    FOREIGN KEY(loc_id) REFERENCES facility(loc_id)
+);
+
+CREATE TABLE IF NOT EXISTS variant_type(
+	variant_id INT AUTO_INCREMENT NOT NULL,
+    variant_name VARCHAR(20) DEFAULT 'UNKNOWN',
+	PRIMARY KEY(variant_id)
+);
+
+CREATE TABLE IF NOT EXISTS infection(
+	p_id INT NOT NULL,
+    idate DATE,
+    type INT NOT NULL,
+    PRIMARY KEY(p_id, idate),
+    FOREIGN KEY(p_id) REFERENCES person(p_id),
+    FOREIGN KEY(type) REFERENCES variant_type(variant_id)
+);
+
+CREATE TABLE IF NOT EXISTS inventory(
+	loc_id INT NOT NULL,
+    vac_id SMALLINT NOT NULL,
+    quantity BIGINT DEFAULT 0,
+    PRIMARY KEY(loc_id, vac_id),
+    FOREIGN KEY(loc_id) REFERENCES facility(loc_id),
+    FOREIGN KEY(vac_id) REFERENCES vaccine(vac_id)
+);
+
+CREATE TABLE IF NOT EXISTS shipment(
+	ship_id INT AUTO_INCREMENT NOT NULL,
+    loc_id INT NOT NULL,
+    sdate DATE NOT NULL,
+    vac_id SMALLINT NOT NULL,
+    quantity BIGINT NOT NULL,
+    PRIMARY KEY(ship_id),
+    FOREIGN KEY(loc_id) REFERENCES facility(loc_id),
+    FOREIGN KEY(vac_id) REFERENCES vaccine(vac_id)
+);
+
+CREATE TABLE IF NOT EXISTS transfer(
+	trans_id INT AUTO_INCREMENT NOT NULL,
+    from_loc INT NOT NULL,
+    to_loc INT NOT NULL,
+    tdate DATE NOT NULL,
+    vac_id SMALLINT NOT NULL,
+    quantity BIGINT NULL,
+	PRIMARY KEY(trans_id),
+    FOREIGN KEY(from_loc) REFERENCES facility(loc_id),
+    FOREIGN KEY(to_loc) REFERENCES facility(loc_id),
+    FOREIGN KEY(vac_id) REFERENCES vaccine(vac_id)
+);
