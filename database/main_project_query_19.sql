dc83dc64a8ea5c01a2853eb67db8bf52605a8c35
heheheliu <70963007+heheheliu@users.noreply.github.com>
2021-08-06 14:11:02 -0400

    Update main_project_query_19.sql

--- database/main_project_query_19.sql
+++ database/main_project_query_19.sql
@@ -5,4 +5,4 @@ JOIN person p ON p.p_id = hw.p_id
 JOIN citizen c ON hw.p_id = c.p_id 
 JOIN work_history wh ON hw.emp_id = wh.emp_id         
 JOIN facility f ON wh.loc_id = f.loc_id
-WHERE f.type = 'special installment'
+WHERE f.loc_id = 2
